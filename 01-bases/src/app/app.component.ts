415246a8f866e0df2c63b4096e4426a6996c484d
olga conesa <olgaconesaboj@gmail.com>
2021-04-05 10:59:54 +0200

    Primera app en angular "Contador"

---
+++ 01-bases/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: 'app.component.html'
+})
+export class AppComponent {
+  titulo: string = 'Contador App';
+  numero: number = 10;
+}
