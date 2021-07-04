4befdd6ccd4ae659121f716662e39be0f6d23cf6
olga conesa <olgaconesaboj@gmail.com>
2021-07-04 10:37:15 +0200

    ngModel

--- 01-bases/src/app/dbz/main-page/main-page.component.ts
+++ 01-bases/src/app/dbz/main-page/main-page.component.ts
@@ -1,15 +1,22 @@
 import { Component } from '@angular/core';
 
+interface Personaje {
+  nombre: string;
+  poder: number;
+}
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html'
 })
 export class MainPageComponent {
 
+  nuevo: Personaje = {
+    nombre: 'Trucks',
+    poder: 14000
+  }
 
   agregar() {
-
-    console.log('prueba')
+    console.log( this.nuevo )
   }
 
 }
