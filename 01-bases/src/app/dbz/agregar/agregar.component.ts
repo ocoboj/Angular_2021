76472175446e7eadc954389ac25b280865fd42e2
olga conesa <olgaconesaboj@gmail.com>
2021-09-12 10:09:45 +0200

    Creación servicios

--- 01-bases/src/app/dbz/agregar/agregar.component.ts
+++ 01-bases/src/app/dbz/agregar/agregar.component.ts
@@ -1,5 +1,6 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Personaje } from '../interfaces/dbz.interfaces';
+import { DbzService } from '../services/dbz.service';
 
 @Component({
   selector: 'app-agregar',
@@ -12,14 +13,16 @@ export class AgregarComponent {
     poder: 0
   }
 
-  @Output() onNuevoPersonaje: EventEmitter<Personaje> = new EventEmitter();
+  constructor(private dbzService: DbzService){}
+
+  //@Output() onNuevoPersonaje: EventEmitter<Personaje> = new EventEmitter();
 
   agregar() {
-    if (this.nuevo.nombre.trim().length == 0 ){
-      return;
-    }
-    console.log( this.nuevo );
-    this.onNuevoPersonaje.emit( this.nuevo );
+    if (this.nuevo.nombre.trim().length == 0 ){return;}
+
+    //this.onNuevoPersonaje.emit( this.nuevo );
+
+    this.dbzService.agregarPersonaje(this.nuevo);
 
     this.nuevo = {
       nombre: '',
--- 01-bases/src/app/dbz/dbz.module.ts
+++ 01-bases/src/app/dbz/dbz.module.ts
@@ -6,6 +6,8 @@ import { MainPageComponent } from '../dbz/main-page/main-page.component';
 import { PersonajesComponent } from './personajes/personajes.component';
 import { AgregarComponent } from './agregar/agregar.component';
 
+import { DbzService } from './services/dbz.service';
+
 
 
 
@@ -21,6 +23,9 @@ import { AgregarComponent } from './agregar/agregar.component';
   imports: [
     CommonModule,
     FormsModule
+  ],
+  providers: [
+     DbzService
   ]
 })
 export class DbzModule { }
