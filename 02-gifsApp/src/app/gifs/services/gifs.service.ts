87781b14894a3e39e0092cdf2335ff849e8ec881
olga conesa <olgaconesaboj@gmail.com>
2021-09-26 10:54:14 +0200

    HttpParams

--- 02-gifsApp/src/app/gifs/services/gifs.service.ts
+++ 02-gifsApp/src/app/gifs/services/gifs.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Gif, SearchGifsResponse } from '../interfaces/gifs.interfaces';
 
@@ -10,6 +10,8 @@ export class GifsService {
 
   private apiKey: string = '5yMxQPxanQWZykp9TsbsccNSRo96QFF6';
 
+  private serviceUrl: string = 'https://api.giphy.com/v1/gifs';
+
   private _historial: string[] = [];
 
   public resultados: Gif[] = [];
@@ -19,15 +21,15 @@ export class GifsService {
     return [...this._historial];
   }
 
-  constructor( private http: HttpClient) {
+  constructor(private http: HttpClient) {
 
-    this._historial = JSON.parse( localStorage.getItem('historial')!) || [];
+    this._historial = JSON.parse(localStorage.getItem('historial')!) || [];
 
-    this.resultados = JSON.parse( localStorage.getItem('resultados')!) || [];
+    this.resultados = JSON.parse(localStorage.getItem('resultados')!) || [];
 
-   /*  if ( localStorage.getItem('historial') ){
-      this._historial = JSON.parse( localStorage.getItem('historial')!);
-    } */
+    /*  if ( localStorage.getItem('historial') ){
+       this._historial = JSON.parse( localStorage.getItem('historial')!);
+     } */
 
   }
 
@@ -41,17 +43,21 @@ export class GifsService {
 
       this._historial = this._historial.splice(0, 10);
 
-      localStorage.setItem('historial', JSON.stringify( this._historial ));
+      localStorage.setItem('historial', JSON.stringify(this._historial));
 
     }
-    
-    this.http.get<SearchGifsResponse>(`https://api.giphy.com/v1/gifs/search?api_key=5yMxQPxanQWZykp9TsbsccNSRo96QFF6&q=${ query }&limit=10`)
-    .subscribe( (resp) => {
-      console.log(resp.data);
-      this.resultados = resp.data;
-      localStorage.setItem('resultados', JSON.stringify( this.resultados ));
-    })
+
+    const params = new HttpParams()
+      .set('api_key', this.apiKey)
+      .set('limit', '10')
+      .set('q', query)
+
+    this.http.get<SearchGifsResponse>(`${ this.serviceUrl }/search`, { params })
+      .subscribe((resp) => {
+        console.log(resp.data);
+        this.resultados = resp.data;
+        localStorage.setItem('resultados', JSON.stringify(this.resultados));
+      })
   }
 
 }
- 
