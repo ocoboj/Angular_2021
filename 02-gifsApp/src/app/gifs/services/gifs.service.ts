5fec897c9180a9df16b81dd4513e9dd005e5a470
olga conesa <olgaconesaboj@gmail.com>
2021-09-26 09:57:32 +0200

    Colocando un tipado a las peticiones http

--- 02-gifsApp/src/app/gifs/services/gifs.service.ts
+++ 02-gifsApp/src/app/gifs/services/gifs.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Gif, SearchGifsResponse } from '../interfaces/gifs.interfaces';
 
 
 @Injectable({
@@ -11,7 +12,7 @@ export class GifsService {
 
   private _historial: string[] = [];
 
-  public resultados: any[] = [];
+  public resultados: Gif[] = [];
 
   get historial() {
 
@@ -31,11 +32,12 @@ export class GifsService {
       this._historial = this._historial.splice(0, 10);
     }
     
-    this.http.get(`https://api.giphy.com/v1/gifs/search?api_key=5yMxQPxanQWZykp9TsbsccNSRo96QFF6&q=${ query }&limit=10`)
-    .subscribe( (resp: any) => {
+    this.http.get<SearchGifsResponse>(`https://api.giphy.com/v1/gifs/search?api_key=5yMxQPxanQWZykp9TsbsccNSRo96QFF6&q=${ query }&limit=10`)
+    .subscribe( (resp) => {
       console.log(resp.data);
       this.resultados = resp.data;
     })
   }
 
 }
+ 
