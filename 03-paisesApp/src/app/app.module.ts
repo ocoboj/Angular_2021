2c66bd10e5584de4da96ec7052b1a0395e010f8d
olga conesa <olgaconesaboj@gmail.com>
2021-10-03 09:54:18 +0200

    RouterModule-app Paises

--- 03-paisesApp/src/app/app.module.ts
+++ 03-paisesApp/src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { PaisModule } from './pais/pais.module';
 import { SharedModule } from './shared/shared.module';
@@ -11,8 +13,9 @@ import { SharedModule } from './shared/shared.module';
   ],
   imports: [
     BrowserModule,
+    AppRoutingModule,
     PaisModule,
-    SharedModule
+    SharedModule,
   ],
   bootstrap: [AppComponent]
 })
